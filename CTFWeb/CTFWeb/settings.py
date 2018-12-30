aa17da2b641a2e8b5a1542d3fc7f780c4709bd90
lightingwm <chenwm_@outlook.com>
2018-12-30 23:49:34 +0800

    修改目录

--- CTFWeb/CTFWeb/settings.py
+++ CTFWeb/CTFWeb/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3@e+*w0x=!cof*9qqrb86bfb)qssekhv7gyfgg)9=nd&a_efgk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
